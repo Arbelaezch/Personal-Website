bbedc374ca82d54df601708fd27263618f38e501
Arbelaezch <arbelaezch@gmail.com>
2021-09-17 04:01:39 -0600

    portfolio fixes

---
+++ Website/pages/portfolio/portfolio.js
@@ -0,0 +1,28 @@
+let myLibrary = [];
+
+//Book Constructor
+function Book(title, author) {
+	this.title = title;
+	this.author = author;
+}
+
+function addBookToLibrary(title, author) {
+	
+
+}
+
+//const addBtn = document.getElementById('add');
+
+
+
+const addBtn = document.querySelector("#add");
+
+
+
+addBtn.addEventListener('click', (e) => 
+{
+	const title = document.getElementById('title').value;
+	const author = document.getElementById('author').value;
+	addBookToLibrary(title, author);
+
+});
