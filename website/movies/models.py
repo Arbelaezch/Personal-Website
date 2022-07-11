65a1063ebe50630cb0e2713a7a57492234145796
Christian Arbelaez <christian.arbelaez@Administrators-Mac-mini-3.local>
2022-07-11 15:25:52 -0600

    Adds movie models

--- website/movies/models.py
+++ website/movies/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+DECADE_LIST = [
+    ('1900s', '1900'),
+    ('1930s', '1930'),
+    ('1940s', '1940'),
+    ('1950s', '1950'),
+    ('1960s', '1960'),
+    ('1970s', '1970'),
+    ('1980s', '1980'),
+    ('1990s', '1990'),
+    ('2000s', '2000'),
+    ('2010s', '2010'),
+    ('2020s', '2020'),
+]
+
+
+class Film(models.Model):
+    title = models.CharField(max_length=200)
+    year = models.DateTimeField('date published')
+    director = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+
+class Decade(models.Model):
+    decade = forms.ChoiceField(choices=DECADE_LIST)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
--- website/movies/urls.py
+++ website/movies/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.film, name='film'),
+    path('decade/', views.decade, name='decade'),
+    path('movie/', views.movie, name='movie'),
 ]
