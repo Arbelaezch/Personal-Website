d9ef94f1232a6e3a417187d758272b8b618a2e16
Arbelaezch <arbelaezch@gmail.com>
2022-07-17 00:59:18 -0600

    Adds portfolio images

--- website/portfolio/views.py
+++ website/portfolio/views.py
@@ -4,5 +4,7 @@ from django.shortcuts import render
 def portfolio(request):
     
     context = {
+        'title': 'Portfolio',
+        
     }
     return render(request, 'portfolio/portfolio.html', context)
