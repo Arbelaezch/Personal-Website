51c133b15cb6ad1d3ce04cb55b08c63083acba7c
Arbelaezch <christianarbelaez@Christians-MacBook-Pro.local>
2023-04-02 16:37:58 -0600

    Updates Article.subject to choices

--- website/blog/models.py
+++ website/blog/models.py
@@ -4,10 +4,18 @@ from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
 
 class Article(models.Model):
+    SUBJECT_CHOICES = [
+        ("Programming", "Programming"),
+        ("Film", "Film"),
+        ("Music", "Music"),
+    ]
+    
+    
     title = models.CharField(max_length=200)
     body = RichTextUploadingField(config_name='portal_config')
-    subject = models.CharField(max_length=50)
+    subject = models.CharField(choices=SUBJECT_CHOICES, max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
 
     def __str__(self):
