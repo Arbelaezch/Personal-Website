857a5f915caf574993fe2767ea8594ffb2571794
Christian Arbelaez <arbelaezch@gmail.com>
2024-12-20 13:35:01 -0700

    Project detail page hooked up

--- website/portfolio/urls.py
+++ website/portfolio/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from website import settings
 from django.conf.urls.static import static
+from .views import ProjectListView, ProjectDetailView
 
 from . import views
 
 urlpatterns = [
     path('', views.portfolio, name='portfolio'),
     path('', views.portfolio_base, name='portfolio_base'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
 ]
